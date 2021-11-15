bb273fd5a231af943d286e9a946cf1dd921875c1
Dastan707 <80236839+Dastan707@users.noreply.github.com>
2021-11-15 17:07:55 +0600

    new updater

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, autoUpdater } = require('electron');
 
-require('update-electron-app')();
+// require('update-electron-app')();
 
 
 const init = () => {
@@ -11,6 +11,25 @@ const init = () => {
 
     mainWindow.webContents.loadURL('data:text/plain;charset=utf-8,' + encodeURI(content));
 
+    autoUpdater.checkForUpdates()
+
 }
 
 app.on('ready', init);
+
+autoUpdater.on('error', err => {
+    console.log('updated error');
+    console.log(err);
+})
+
+autoUpdater.on('checking-for-update', () => {
+    console.log('checking-for-update')
+  })
+
+  autoUpdater.on('update-available', () => {
+    console.log('update-available; downloading...')
+  })
+
+  autoUpdater.on('update-not-available', () => {
+    console.log('update-not-available')
+  })
